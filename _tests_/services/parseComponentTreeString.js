6834483949b6b34721a16441a6cbc71861b9cf32
Jonny <joropeza@gmail.com>
2016-09-27 07:06:45 -0700

    robustify test for component mapping

--- _tests_/services/parseComponentTreeString.js
+++ _tests_/services/parseComponentTreeString.js
@@ -2,11 +2,36 @@ import { expect } from 'chai';
 
 import functionToTest from '../../src/services/parseComponentTreeString';
 
+const componentTypes = {
+    SINGLE_COMPONENT: 'SINGLE_COMPONENT',
+};
+
 describe('(parse component string)', function() {
     it('should find a single component', function() {
         const data = 'myComponent';
         const results = functionToTest(data);
 
         expect(results.length).to.equal(1);
+    });
+    it('should find a two components', function() {
+        const data = 'page+content';
+
+        const expectedResults = {
+            page: {
+                children: {
+                    content: {
+                        usage: componentTypes.SINGLE_COMPONENT,
+                    },
+                },
+            },
+            content: {
+
+            },
+        };
+
+        const results = functionToTest(data);
+
+        expect(results.length).to.equal(2);
+        expect(results).to.equal(expectedResults);
     });
 });
