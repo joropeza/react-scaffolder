e3fd9b354e71e8f53bf6561d7625c428d2c245f9
Jonny <joropeza@gmail.com>
2016-10-02 06:44:39 +0800

    initial hacky TDD solution to parsing strings

--- _tests_/services/parseComponentTreeString.js
+++ _tests_/services/parseComponentTreeString.js
@@ -1,9 +1,11 @@
 import { expect } from 'chai';
+import _ from 'lodash';
 
 import functionToTest from '../../src/services/parseComponentTreeString';
 
-const componentTypes = {
+const componentUsages = {
     SINGLE_COMPONENT: 'SINGLE_COMPONENT',
+    ARRAYED_COMPONENT: 'ARRAYED_COMPONENT',
 };
 
 describe('(parse component string)', function() {
@@ -11,27 +13,101 @@ describe('(parse component string)', function() {
         const data = 'myComponent';
         const results = functionToTest(data);
 
-        expect(results.length).to.equal(1);
+        const expectedResults = {
+            myComponent: {
+                children: {},
+            },
+        };
+
+        expect((_.values(results)).length).to.equal((_.values(expectedResults)).length);
+        expect(results).to.deep.equal(expectedResults);
     });
-    it('should find a two components', function() {
+    it('should create a toplevel component with three subs', function() {
+        const data = 'body+(header,[listItem],footer+(copyright,watermark))';
+
+        const expectedResults = {
+            body: {
+                children: {
+                    header: {
+                        usage: componentUsages.SINGLE_COMPONENT,
+                    },
+                    listItem: {
+                        usage: componentUsages.ARRAYED_COMPONENT,
+                    },
+                    footer: {
+                        usage: componentUsages.SINGLE_COMPONENT,
+                    },
+                },
+            },
+            header: {
+                children: {},
+            },
+            listItem: {
+                children: {},
+            },
+            footer: {
+                children: {
+                    copyright: {
+                        usage: componentUsages.SINGLE_COMPONENT,
+                    },
+                    watermark: {
+                        usage: componentUsages.SINGLE_COMPONENT,
+                    },
+                },
+            },
+            copyright: {
+                children: {},
+            },
+            watermark: {
+                children: {},
+            },
+        };
+
+        const results = functionToTest(data);
+
+        expect((_.values(results)).length).to.equal((_.values(expectedResults)).length);
+        expect(results).to.deep.equal(expectedResults);
+    });
+    it('should find two components', function() {
         const data = 'page+content';
 
         const expectedResults = {
             page: {
                 children: {
                     content: {
-                        usage: componentTypes.SINGLE_COMPONENT,
+                        usage: componentUsages.SINGLE_COMPONENT,
                     },
                 },
             },
             content: {
-
+                children: {},
             },
         };
 
         const results = functionToTest(data);
 
-        expect(results.length).to.equal(2);
-        expect(results).to.equal(expectedResults);
+        expect((_.values(results)).length).to.equal((_.values(expectedResults)).length);
+        expect(results).to.deep.equal(expectedResults);
+    });
+    it('should find two components with one of them mapped', function() {
+        const data = 'list+[listItem]';
+
+        const expectedResults = {
+            list: {
+                children: {
+                    listItem: {
+                        usage: componentUsages.ARRAYED_COMPONENT,
+                    },
+                },
+            },
+            listItem: {
+                children: {},
+            },
+        };
+
+        const results = functionToTest(data);
+
+        expect((_.values(results)).length).to.equal((_.values(expectedResults)).length);
+        expect(results).to.deep.equal(expectedResults);
     });
 });
--- src/services/parseComponentTreeString.js
+++ src/services/parseComponentTreeString.js
@@ -1,5 +1,102 @@
+import parenthesis from 'parenthesis';
+import _ from 'lodash';
+
+const componentUsages = {
+    SINGLE_COMPONENT: 'SINGLE_COMPONENT',
+    ARRAYED_COMPONENT: 'ARRAYED_COMPONENT',
+};
+
+const removeBrackets = aString =>
+    aString.replace('[', '').replace(']', '');
+
+const getReferenceIdFromParenthesisString = aString =>
+    Number(aString.replace('(\\', '').replace(')', ''));
+
+const getUsage = aString =>
+    (aString.startsWith('[')) ? componentUsages.ARRAYED_COMPONENT : componentUsages.SINGLE_COMPONENT;
+
+const breakApartChildString = aString =>
+    aString.split('+');
+
+const getNextCharecter = (aString, aSubstring) =>
+    (aString.substr(aString.indexOf(aSubstring) + aSubstring.length)).charAt(0);
+
+const createBaseComponentTree = (parendString) => {
+    // create the base component tree
+    const componentTree = {};
+    _.forEach(parendString, (pstring) => {
+        const plusBrokenPString = pstring.split('+');
+        _.forEach(plusBrokenPString, (componentName) => {
+            if (!componentName.startsWith('(\\')) {
+                const commaBrokenPString = componentName.split(',');
+                _.forEach(commaBrokenPString, (subComponentName) => {
+                    const finalComponentName = removeBrackets(subComponentName);
+                    componentTree[finalComponentName] = { children: {} };
+                });
+            }
+        });
+    });
+    _.forEach(parendString, (pstring) => {
+        const plusBrokenPString = pstring.split('+');
+        const components = plusBrokenPString[0].split(',');
+        _.forEach(components, (component) => {
+            const componentChildren = [];
+            for (let i = 1; i < plusBrokenPString.length; i += 1) {
+                const childSegment = plusBrokenPString[i];
+                if (childSegment.startsWith('(\\')) {
+                    // console.log('is paren reffed', getReferenceIdFromParenthesisString(childSegment));
+                }
+                const parsedChildSegment = (childSegment.startsWith('(\\')) ?
+                    parendString[getReferenceIdFromParenthesisString(childSegment)] :
+                    childSegment;
+                const commaBrokenChilds = parsedChildSegment.split(',');
+                _.forEach(commaBrokenChilds, childString => {
+                    // if a child has a plus, it needs to be broken up too
+                    // could this be done recursively?
+
+                    const plusBrokenChildString = pstring.split('+');
+
+                    const child = (plusBrokenChildString.length === 1) ?
+                        childString :
+                        breakApartChildString(childString)[0];
+
+                    const nextCharecter = getNextCharecter(pstring, component);
+                    if (nextCharecter === '+') {
+                        componentChildren.push(child);
+                    }
+                });
+            }
+            console.log(`i am ${component} and my ${componentChildren.length} children are ${componentChildren}`);
+            _.forEach(componentChildren, child => {
+                if(componentTree[component]) {
+                    componentTree[component].children[removeBrackets(child)] = {
+                        usage: getUsage(child)
+                    };
+                }
+            });
+        });
+    });
+
+    // go back and see who had a plus something
+    console.log(parendString);
+
+    return componentTree;
+};
+
 const parseComponentTreeString = (componentTreeString) => {
-    return [componentTreeString];
+    const parendString = parenthesis(
+        componentTreeString, {
+            brackets: ['()'],
+            escape: '\\',
+            flat: true,
+        }
+    );
+    // console.log('paren broken array is', parendString);
+
+    const componentTree = createBaseComponentTree(parendString);
+    // console.log('component map is', componentTree);
+
+    return componentTree;
 };
 
 export default parseComponentTreeString;
