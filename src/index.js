71422029d5daca9c34d1f8aafab4b788cd8b6868
Jonny <joropeza@gmail.com>
2016-11-25 08:56:18 -0800

    provide validation of path param

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ import writeComponent from './services/writeComponent';
 
 // const directory = process.cwd();
 
+const cleanupPath = (path) => {
+    if (!path) {
+        return './';
+    }
+    let cleanedupPath = path;
+    // sourced from http://stackoverflow.com/a/11531417
+    const lastChar = cleanedupPath.substr(-1); // Selects the last character
+    if (lastChar !== '/') {         // If the last character is not a slash
+        cleanedupPath += '/';            // Append a slash to it.
+    }
+    return cleanedupPath;
+};
+
 program
   .version('0.0.1')
   .option('-c, --componentTree [name]', 'Add a component [name]')
@@ -18,12 +31,12 @@ const componentTreeString = program.componentTree;
 const reactAst = parseComponentTreeString(componentTreeString);
 const components = renderFunctionalComponents(reactAst);
 
-console.log(components);
-
 // const outputPath = program.outputPath || './';
 
+const path = cleanupPath(program.path);
+
 const writeResults = _.reduce(components, (errors, component, componentName) => {
-    const newError = writeComponent(component, componentName, program.path || './');
+    const newError = writeComponent(component, componentName, path);
     if (newError) { errors.push(newError); }
     return errors;
 }, []);
